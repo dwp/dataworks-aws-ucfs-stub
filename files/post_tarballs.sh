7adba78f96e2043eead7c278942dcf6efe92e487
Matt Burgess <549318+mattburgess@users.noreply.github.com>
2020-11-02 14:59:08 +0000

    DW-5074: Fix typo

--- files/post_tarballs.sh
+++ files/post_tarballs.sh
@@ -5,4 +5,4 @@ aws s3 sync s3://${s3_input_bucket}/${s3_input_prefix} /srv/data/export
 
 echo "Sending tarballs to ingestion server"
 today=$(date "+%Y-%m-%d")
-aws --endpoint https://${tarball_ingester_s3_endpoint} sync /srv/data/export s3://${tarball_ingester_s3_bucket}/$today/
+aws --endpoint https://${tarball_ingester_s3_endpoint} s3 sync /srv/data/export s3://${tarball_ingester_s3_bucket}/$today/
