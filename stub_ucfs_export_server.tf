7eb10e79f06cf82eec4119220e456f012b76b99b
simoneyong <simon.eyong@engineering.digital.dwp.gov.uk>
2023-06-12 13:52:30 +0100

    updating fix

--- stub_ucfs_export_server.tf
+++ stub_ucfs_export_server.tf
@@ -675,7 +675,8 @@ resource "aws_s3_object" "stub_ucfs_export_server_post_tarballs_script" {
     s3_input_prefix              = "business-data/tarballs-mongo/ucdata/"
     tarball_ingester_s3_endpoint = aws_route53_record.tarball_ingester.name
   #  tarball_ingester_s3_endpoint = data.terraform_remote_state.tarball_ingester.outputs.tarball_ingester_fqdn
-    tarball_ingester_s3_bucket   = data.terraform_remote_state.tarball_ingester.outputs.tarball_ingester_minio_s3_bucket_name
+    tarball_ingester_s3_bucket   = var.minio_s3_bucket_name
+    # tarball_ingester_s3_bucket   = data.terraform_remote_state.tarball_ingester.outputs.tarball_ingester_minio_s3_bucket_name
   })
 
   tags = merge(
